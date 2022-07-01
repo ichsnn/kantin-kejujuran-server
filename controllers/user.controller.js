45c92fd9a4de64ff4ea3ab85ef7af27021813751
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-07-01 20:57:06 +0700

    auth

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,43 @@
 const { User } = require("../models");
 
 const userAuth = async (req, res) => {
-  const id = req.userId
+  const id = req.userId;
   const user = await User.findOne({
     where: {
       id: id,
     },
   });
 
-  if(!user) {
-    return res.status(400).json({error: true, message: 'User not found'});
+  if (!user) {
+    return res.status(400).json({ error: true, message: "User not found" });
   }
 
   return res.json({
     id: user.id,
     name: user.name,
-  })
+  });
+};
+
+const home = async (req, res) => {
+  const id = req.userId;
+  const user = await User.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ error: true, message: "User not found" });
+  }
+
+  return res.json({
+    id: user.id,
+    name: user.name,
+    balance: user.balance,
+  });
 };
 
 module.exports = {
   userAuth,
+  home
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const { verifyToken } = require("../middlewares/authJwt");
 
 const userRoutes = Router();
 const controller = require("../controllers/user.controller");
 
-userRoutes.get('/auth', verifyToken, controller.userAuth)
+userRoutes.get("/auth", verifyToken, controller.userAuth);
+userRoutes.get("/home", verifyToken, controller.home);
 
 module.exports = userRoutes;
